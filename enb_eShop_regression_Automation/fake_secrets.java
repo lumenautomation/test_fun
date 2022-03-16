e07d02a291e4ce8fd8c545c6918830d17e744528
ac83266 <bret.masters@lumen.com>
2022-03-16 15:39:03 -0600

    Added more test cases

---
+++ enb_eShop_regression_Automation/fake_secrets.java
@@ -0,0 +1,4 @@
+dbURI ="mongodb://BMP_dba:BMP_dba_fake123@fakemong01.dev.intranet:26000"
+HostName ="jdbc:oracle:thin:@thisisfake.test.intranet:1521/ENSDEP1";
+	        	  UserName ="fakeqa";
+	        	  PassWord ="fakedep1";
---
+++ enb_eShop_regression_Automation/search_4_strings.java
@@ -0,0 +1,1 @@
+autoregr@lumen.com
